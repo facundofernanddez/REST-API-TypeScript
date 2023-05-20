b694a4888c468e06f4e1e4a97093ee41d0dabc85
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-20 18:29:51 -0300

    create router

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ app.use(express.json());
 
 const PORT = 4000;
 
+app.get("/ping", (_req, res) => {
+  console.log("someone piged here");
+  res.send("pong!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
