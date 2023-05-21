6d640ee7d1fb3d597bcb3cc3dfdc45bc35ffa688
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-20 22:48:40 -0300

    create services and types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+
+//routes
+import diaryRouter from "./routes/diaries";
+
 const app = express();
 app.use(express.json());
 
@@ -8,6 +12,8 @@ app.get("/ping", (_req, res) => {
   console.log("someone piged here");
   res.send("pong!");
 });
+
+app.use("/api/diaries", diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
